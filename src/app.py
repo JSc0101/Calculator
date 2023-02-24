aa20c9acdfc804d3f8c9b3bf7d0c21eaf1e00947
Sebastian009w <ia3266505@gmail.com>
2023-02-23 21:17:29 -0500

    button definidos --

--- src/app.py
+++ src/app.py
@@ -7,17 +7,17 @@ display = Entry(root)
 display.grid(row=1, columnspan=6, sticky=W+E)
 
 # - Buttons
-Button(root, text="1").grid(row=2, colum=0)
-Button(root, text="2").grid(row=2, colum=0)
-Button(root, text="3").grid(row=2, colum=0)
+Button(root, text="1").grid(row=2, column=0)
+Button(root, text="2").grid(row=2, column=0)
+Button(root, text="3").grid(row=2, column=0)
 
-Button(root, text="4").grid(row=2, colum=0)
-Button(root, text="5").grid(row=2, colum=0)
-Button(root, text="6").grid(row=2, colum=0)
+Button(root, text="4").grid(row=3, column=0)
+Button(root, text="5").grid(row=3, column=0)
+Button(root, text="6").grid(row=3, column=0)
 
-Button(root, text="7").grid(row=2, colum=0)
-Button(root, text="8").grid(row=2, colum=0)
-Button(root, text="9").grid(row=2, colum=0)
+Button(root, text="7").grid(row=4, column=0)
+Button(root, text="8").grid(row=4, column=0)
+Button(root, text="9").grid(row=4, column=0)
 
 
 root.mainloop()
